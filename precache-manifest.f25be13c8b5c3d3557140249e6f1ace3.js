7b500444a9b3a1784fdb634d84731efa9a0cc911
Tanya Sharma <tanu282501@gmail.com>
2023-09-28 15:04:40 +0530

    Updates

---
+++ precache-manifest.f25be13c8b5c3d3557140249e6f1ace3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "192b7ee42db49371b256",
+    "url": "/knowMe/static/css/main.e914ebd6.chunk.css"
+  },
+  {
+    "revision": "192b7ee42db49371b256",
+    "url": "/knowMe/static/js/main.94ceb361.chunk.js"
+  },
+  {
+    "revision": "1d41f51e992018f31840",
+    "url": "/knowMe/static/js/runtime~main.5eb5d751.js"
+  },
+  {
+    "revision": "1c14a9ea957a16db1c29",
+    "url": "/knowMe/static/js/2.3ff18bc7.chunk.js"
+  },
+  {
+    "revision": "d476af15383a2a97dba700de95e8b9ba",
+    "url": "/knowMe/index.html"
+  }
+];
