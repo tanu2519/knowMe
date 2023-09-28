67d9b8fea815c325c9aab5f1a12dee2f6bd4a0a4
Tanya Sharma <tanu282501@gmail.com>
2023-09-28 14:48:32 +0530

    start form fix

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,24 +1,59 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import emailjs from '@emailjs/browser';
 
 export default function Contact() {
 
   // emailJs starts
 
-  const form = useRef();
+  // const form = useRef();
+
+  // const sendEmail = (e) => {
+  //   e.preventDefault();
+
+  //   emailjs.sendForm('service_m8figgo', 'template_095q27f', form.current, 'tP4xayNfC5ClXqfso')
+  //     .then((result) => {
+  //       alert("Message sent!");
+  //       form.current.reset();
+  //     }, (error) => {
+  //         console.log(error.text);
+  //     }
+  //     );
+  // };
+
+  const [formData, setFormData] = useState({
+    user_name: "",
+    user_email: "",
+    user_phone: "",
+    message: "",
+  });
 
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_m8figgo', 'template_095q27f', form.current, 'tP4xayNfC5ClXqfso')
+    emailjs.sendForm('service_m8figgo', 'template_095q27f', e.target, 'tP4xayNfC5ClXqfso')
       .then((result) => {
         alert("Message sent!");
-        form.current.reset();
-      }, (error) => {
-          console.log(error.text);
-      }
-      );
+        // Clear form fields after successful submission
+        setFormData({
+          user_name: "",
+          user_email: "",
+          user_phone: "",
+          message: "",
+        });
+      })
+      .catch((error) => {
+        console.log(error.text);
+      });
   };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
 
  // emailJs ends
 
@@ -61,9 +96,9 @@ export default function Contact() {
           </div>
         </div>
         <form
-          ref={form}
           onSubmit={sendEmail}
-          className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+          className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
+        >
           <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
             Hire Me
           </h2>
@@ -75,6 +110,8 @@ export default function Contact() {
               type="text"
               id="name"
               name="user_name"
+              value={formData.user_name}
+              onChange={handleInputChange}
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
             />
           </div>
@@ -86,25 +123,28 @@ export default function Contact() {
               type="email"
               id="email"
               name="user_email"
-              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"   
+              value={formData.user_email}
+              onChange={handleInputChange}
+              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
             />
           </div>
           <div className="relative mb-4">
-            <label
-              htmlFor="message"
-              className="leading-7 text-sm text-gray-400">
+            <label htmlFor="message" className="leading-7 text-sm text-gray-400">
               Message
             </label>
             <textarea
               id="message"
               name="message"
+              value={formData.message}
+              onChange={handleInputChange}
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
             />
           </div>
           <input
             type="submit"
             value="Send"
-            className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"/>
+            className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+          />
         </form>
       </div>
     </section>
